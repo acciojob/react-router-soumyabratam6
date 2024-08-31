491e9062cf12c2c22a3a72e92d75af29d70fafbc
soumyabrata <soumyabratam6@gmail.com>
2024-08-31 08:00:14 +0530

    all file add..

--- src/components/App.js
+++ src/components/App.js
@@ -4,7 +4,7 @@ import './../styles/App.css';
 import Home from "./Home";
 import About from "./About";
 
-import { BrowserRouter, Link, Route, Routes,Switch } from "react-router-dom";
+import { BrowserRouter, Link, Route, Routes } from "react-router-dom";
 const App = () => {
   return (
     <div>
@@ -13,12 +13,10 @@ const App = () => {
           <li><Link to='/'>Home</Link></li>
           <li><Link to='/about'>About</Link></li>
         </ul>
-        <Switch>
           <Routes>
           <Route path ="/" element={< Home/>} />
           <Route path="/about" element={<About/>} />
           </Routes>
-        </Switch>
         </BrowserRouter>
     </div>
   )
